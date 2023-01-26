8c8fd147da12baef747e943847372904a8b2ffca
ChagasJG40 <harrisonbchagas@gmail.com>
2023-01-26 19:20:48 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let itemName = 'Pen'
+let itemPrice = 3
+let itemAvailable = true
+let itemColor = 'Red'
+
+let pen= {
+itemName: 'Pen',
+itemPrice: 3,
+itemAvailable: true,
+itemColor: 'Red'
+}
+pen.itemColor = 'green'
+
+console.log(pen)
