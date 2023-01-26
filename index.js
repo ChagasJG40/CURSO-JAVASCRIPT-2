9784686e4e334216f39a6d598343fb00ab68701e
ChagasJG40 <harrisonbchagas@gmail.com>
2023-01-26 19:40:08 -0400

    atualização

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-let itemName = 'Pen'
-let itemPrice = 3
-let itemAvailable = true
-let itemColor = 'Red'
-
-let pen= {
-itemName: 'Pen',
-itemPrice: 3,
-itemAvailable: true,
-itemColor: 'Red'
-}
-pen.itemColor = 'green'
-
-console.log(pen)
+let friends = [ 'João', 'jayderso','caio', 'Rodrigo']
+friends[3] = 'Diego'
+console.log(friends)
